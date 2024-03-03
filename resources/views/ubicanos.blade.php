cfb32bae98a4cd90cd728161538980388cbf264e
Javier1239 <penajavier535@gmail.com>
2024-03-02 20:38:36 -0600

    Se actualiza la pagina de ubicanos

--- resources/views/ubicanos.blade.php
+++ resources/views/ubicanos.blade.php
@@ -4,14 +4,14 @@
     <div class="page-style-a">
         <div class="container">
             <div class="page-intro">
-                <h2>Contact</h2>
+                <h2>Ubicanos</h2>
                 <ul class="bread-crumb">
                     <li class="has-separator">
                         <i class="ion ion-md-home"></i>
-                        <a href="home.html">Home</a>
+                        <a  href="{{ route('index')}}">Home</a>
                     </li>
                     <li class="is-marked">
-                        <a href="contact.html">Contact</a>
+                        <a href="{{ route('conocenos')}}">Conocenos</a>
                     </li>
                 </ul>
             </div>
@@ -24,70 +24,67 @@
             <div class="row">
                 <div class="col-lg-6 col-md-6 col-sm-12">
                     <div class="touch-wrapper">
-                        <h1 class="contact-h1">Get In Touch With Us</h1>
+                        <h1 class="contact-h1">Ponte en contacto con nosotros</h1>
                         <form>
                             <div class="group-inline u-s-m-b-30">
                                 <div class="group-1 u-s-p-r-16">
-                                    <label for="contact-name">Your Name
+                                    <label for="contact-name">Nombre
                                         <span class="astk">*</span>
                                     </label>
-                                    <input type="text" id="contact-name" class="text-field" placeholder="Name">
+                                    <input type="text" id="contact-name" class="text-field" placeholder="Nombre">
                                 </div>
                                 <div class="group-2">
-                                    <label for="contact-email">Your Email
+                                    <label for="contact-email">Tu Email
                                         <span class="astk">*</span>
                                     </label>
                                     <input type="text" id="contact-email" class="text-field" placeholder="Email">
                                 </div>
                             </div>
                             <div class="u-s-m-b-30">
-                                <label for="contact-subject">Subject
+                                <label for="contact-subject">Asunto
                                     <span class="astk">*</span>
                                 </label>
-                                <input type="text" id="contact-subject" class="text-field" placeholder="Subject">
+                                <input type="text" id="contact-subject" class="text-field" placeholder="Asunto">
                             </div>
                             <div class="u-s-m-b-30">
-                                <label for="contact-message">Message:</label>
+                                <label for="contact-message">Mensaje:</label>
                                 <textarea class="text-area" id="contact-message"></textarea>
                             </div>
                             <div class="u-s-m-b-30">
-                                <button type="submit" class="button button-outline-secondary">Send Message</button>
+                                <button type="submit" class="button button-outline-secondary">Enviar</button>
                             </div>
                         </form>
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-12">
                     <div class="information-about-wrapper">
-                        <h1 class="contact-h1">Information About Us</h1>
+                        <h1 class="contact-h1">Información acerca de nosotros</h1>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, tempora, voluptate. Architecto aspernatur, culpa cupiditate deserunt dolore eos facere in, incidunt omnis quae quam quos, similique sunt tempore vel vero.
+                        <p>En Chopper Bzr, nos dedicamos a ofrecer una experiencia única a nuestros clientes. Nuestra historia se teje con pasión por el deporte y el compromiso de proporcionar ropa deportiva de calidad que impulse tu rendimiento y resalte tu estilo.</p>
                         </p>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, tempora, voluptate. Architecto aspernatur, culpa cupiditate deserunt dolore eos facere in, incidunt omnis quae quam quos, similique sunt tempore vel vero.
+                        <p>Nos enorgullece destacar por la calidad de nuestros productos, diseñados meticulosamente para satisfacer las demandas de los entusiastas del deporte moderno. Colaboramos con diseñadores apasionados que comparten nuestra visión de fusionar moda y funcionalidad.</p>
                         </p>
                     </div>
                     <div class="contact-us-wrapper">
-                        <h1 class="contact-h1">Contact Us</h1>
+                        <h1 class="contact-h1">Contactanos</h1>
                         <div class="contact-material u-s-m-b-16">
-                            <h6>Location</h6>
-                            <span>4441 Jett Lane</span>
-                            <span>Bellflower, CA 90706</span>
+                            <h6>Ubicación</h6>
+                            <span>C 15 1097 Fracc Nuevo Mirasierra</span>
+                            <span>Saltillo Coahuila, CP 25016</span>
                         </div>
                         <div class="contact-material u-s-m-b-16">
                             <h6>Email</h6>
-                            <span>contact@domain.com</span>
+                            <span>ChopperBzruwu@gmail.com</span>
                         </div>
                         <div class="contact-material u-s-m-b-16">
-                            <h6>Telephone</h6>
-                            <span>+111-444-989</span>
+                            <h6>Celular</h6>
+                            <span>+52 1 844 334 0771</span>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="u-s-p-t-80">
-            <div id="map"></div>
-        </div>
+        </div>       
     </div>
     <!-- Contact-Page /- -->
     @endsection
